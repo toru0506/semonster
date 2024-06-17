9845db9f001670e54b9f20160fda730dca22fed6
Toru <tftech0506@gmail.com>
2024-06-17 14:54:16 +0900

    MainとPlayerの変更

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMonster!");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
